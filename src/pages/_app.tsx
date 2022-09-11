10bef29bcba0e0fab41b371fb1f0fa4f08d0ed47
Michael McGreal <mcgrealife@gmail.com>
2022-09-11 08:13:11 -0400

    Add solution: instantiate queryClient outside of App component. Ref: "unstable-query-client" in https://tkdodo.eu/blog/react-query-fa-qs#why-are-updates-not-shown

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,11 @@ import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-// the solution is to initialize the query client outside of the app component!
+// Solution: instantiate the QueryClient outside of the App component!
 const queryClient = new QueryClient()
 function MyApp({ Component, pageProps: { ...pageProps } }: AppProps) {
-  // Edit: This is the problem!
+  // Edit: This is the problem! see "unstable-query-client" in https://tkdodo.eu/blog/react-query-fa-qs#why-are-updates-not-shown
   // const queryClient = new QueryClient()
-  // see snippet "unstable-query-client" in https://tkdodo.eu/blog/react-query-fa-qs#why-are-updates-not-shown
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
