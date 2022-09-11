2f5b4d1748aec74cbd98df3d0764e60b912bb266
Michael McGreal <mcgrealife@gmail.com>
2022-09-11 08:09:09 -0400

    add solution: move queryClient out of app component. see snippet "unstable-query-client" in https://tkdodo.eu/blog/react-query-fa-qs#why-are-updates-not-shown

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,20 @@
-import '../../styles/globals.css';
-import type { AppProps } from 'next/app';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import '../../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
+// the solution is to initialize the query client outside of the app component!
+const queryClient = new QueryClient()
 function MyApp({ Component, pageProps: { ...pageProps } }: AppProps) {
-  const queryClient = new QueryClient();
-
+  // Edit: This is the problem!
+  // const queryClient = new QueryClient()
+  // see snippet "unstable-query-client" in https://tkdodo.eu/blog/react-query-fa-qs#why-are-updates-not-shown
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
       <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
